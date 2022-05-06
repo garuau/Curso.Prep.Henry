33b9f83e4ed5b190e0eaf370ce9efa383c47846e
garuau <guilleruau483@gmail.com>
2022-05-06 18:05:48 +0200

    second com

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -74,7 +74,7 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  asasaa
 }
 
 function mayorQueCincuenta(num) {
